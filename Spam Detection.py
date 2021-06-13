79a9494525a1b7131442965d004602df9f904655
Gaurav-96B <72096518+Gaurav-96B@users.noreply.github.com>
2021-06-13 21:16:40 +0530

    Update Spam Detection.py

--- Spam Detection.py
+++ Spam Detection.py
@@ -1,9 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[119]:
-
-
 import pandas as pd
 import re
 import nltk
@@ -17,50 +11,10 @@ from sklearn.metrics import confusion_matrix
 import matplotlib.pyplot as plt
 
 
-# # Importing the dataset
-
-# In[121]:
-
-
+# Importing the dataset
 data=pd.read_csv('SMSSpamCollection',sep='\t',names=["label", "message"])
-data
 
-
-# In[122]:
-
-
-data.head(10)
-
-
-# In[123]:
-
-
-data.tail(10)
-
-
-# In[124]:
-
-
-data.shape
-
-
-# In[125]:
-
-
-data.isna().sum()
-
-
-# In[126]:
-
-
-len(data)
-
-
-# # Data Cleaning and Preprocessing
-
-# In[127]:
-
-
+# Data Cleaning and Preprocessing
 nltk.download('stopwords')
 ps = PorterStemmer()
 corpus = []
@@ -71,121 +25,41 @@ for i in range(0, len(data)):
     review = [ps.stem(word) for word in review if not word in stopwords.words('english')] # list comprehension 
     review = ' '.join(review)
     corpus.append(review)
-
-
-# In[128]:
-
-
-corpus[5570]
-
-
-# # Creating the Bag of Words model
-
-# In[129]:
-
-
+    
+# Creating the Bag of Words model
 cv = CountVectorizer(max_features=2500)
 X = cv.fit_transform(corpus).toarray()
-
 y=pd.get_dummies(data['label'])
 y=y.iloc[:,1].values
 
-
-# In[130]:
-
-
-len(X)
-
-
-# In[131]:
-
-
-len(y)
-
-
-# In[134]:
-
-
-X.shape
-
-
-# # Splitting the dataset into the Training set and Test set
-
-# In[135]:
-
-
+# Splitting the dataset into the Training set and Test set
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.20, random_state = 0)
 
 
-# # Training model using Naive bayes classifier
-# 
-#  # 1.GaussianNB
-
-# In[138]:
-
-
+# Training model using Naive bayes classifier
+# 1.GaussianNB
 GN_classifier = GaussianNB()
 GN_classifier.fit(X_train, y_train)
-
-
-# In[139]:
-
-
 GN_score = GN_classifier.score(X_test,y_test)
-GN_score
-
-
-# In[140]:
-
-
 y_GN_pred = GN_classifier.predict(X_test)
-y_GN_pred
 
 
-# # 2.MultinomialNB
-
-# In[141]:
-
-
+# 2.MultinomialNB
 MN_classifier = MultinomialNB()
 MN_classifier.fit(X_train, y_train)
-
-
-# In[142]:
-
-
 MN_score = MN_classifier.score(X_test,y_test)
-MN_score
-
-
-# In[143]:
-
-
 y_MN_pred = MN_classifier.predict(X_test)
-y_MN_pred
 
 
-# # Making the Confusion Matrix
-
-# In[144]:
-
-
+# Making the Confusion Matrix
 GN_cm = confusion_matrix(y_test, y_GN_pred)
 print(GN_cm)
-
-
-# In[145]:
-
 
 MN_cm = confusion_matrix(y_test, y_MN_pred)
 print(MN_cm)
 
 
-# # Compare Both models
-
-# In[146]:
-
-
+# Compare Both models
 models = pd.DataFrame({"GaussianNB": GN_score,"MultinomialNB": MN_score},index=[0])
 models.T.plot.bar(title="Compare different models",legend=False)
 plt.xticks(rotation=0);
